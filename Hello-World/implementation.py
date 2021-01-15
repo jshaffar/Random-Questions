82a177e10d2bfd57a6adec2ffba55ca12b78b3a0
shaffar3 <shaffar3@illinois.edu>
2021-01-15 11:34:41 -0500

    added hello world question

--- Hello-World/implementation.py
+++ Hello-World/implementation.py
@@ -1,8 +1,18 @@
 import time
+COUNT = 1000
 
-start = time.time()
-print("Hello World!")
-end = time.time()
-diff = end - start
+
+average_diff_num = 0
+iter = 1000 
+for f in range(0, iter):
+    start = time.time()
+    print("Hello World!")
+    end = time.time()
+    diff = end - start
+    average_diff_num += diff
+
+average = average_diff_num / iter
+
 with open("Hello-World/results.txt", "w") as file:
-    file.write(str(diff))
+
+    file.write(str(average) + " is the average number of seconds it takes python to print Hello World! with " + str(iter) + " iterations.")
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Random-Questions
 Sometimes I just have a question i want to answer with actual data rather than just making stuff up like I normally do, so this seems like a good place to do it.
+
+##Questions:
+Q1) How long does it take my computer to print "Hello World!"? <break>
+A) 2.7928352355957032e-06 is the average number of seconds it takes python to print Hello World! with 1000 iterations.
+
